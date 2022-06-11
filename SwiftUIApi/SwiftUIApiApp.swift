bd757e99f020371d5500d0c9c3473f833381705a
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-06-12 00:10:56 +0600

    Initial Commit

---
+++ SwiftUIApi/SwiftUIApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiApp.swift
+//  SwiftUIApi
+//
+//  Created by Sopnil Sohan on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
